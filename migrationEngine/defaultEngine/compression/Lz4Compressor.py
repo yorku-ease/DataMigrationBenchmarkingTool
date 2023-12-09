58bc5f3947b5e82f60ab51adc838097babe3a414
fares <hamoudaferes@gmail.com>
2023-12-09 01:27:09 -0500

    seperate migration engine

---
+++ migrationEngine/defaultEngine/compression/Lz4Compressor.py
@@ -0,0 +1,8 @@
+from compression.Compressor import Compressor
+import lz4.frame
+class Lz4Compressor(Compressor):
+    
+    def compress(self,data):
+        return lz4.frame.compress(data)
+    def addFileExtension(self,filename):
+        return filename + ".lz4"
