a3c1111c990d2130f6bf972064cb2fbffdb96c97
Fares Hamouda <fareshamouda@li-65d741cc-3409-11b2-a85c-a5bbb47274fc.ibm.com>
2023-12-27 22:55:20 -0500

    add logsParser unittests

---
+++ deployment/reporter/logsParser/unitTest/TestLogTransformation.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import unittest,json
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+os.chdir(parent_directory)
+
+
+from parser import Parser
+
+from frameworkParser import FrameworkParser
+from performanceBenchmarkParser import PerformanceBenchmarkParser
+from migrationEngineParser import MigrationEngineParser
+
+
+class TestLogTransformation(unittest.TestCase):
+
+    def test_PerformanceBenchmarkParser(self):
+        parser = PerformanceBenchmarkParser("unitTest/performanceBenchmark.log","unitTest/operformanceBenchmark.json","unitTest/operformanceBenchmark.csv")
+        parser.toJsonData()
+        parser.toJsonfile()
+        output_data = json.dumps(parser.toJsonData(), sort_keys=True)
+        with open("unitTest/operformanceBenchmark.json", 'r') as output_file:
+            output_data = json.dumps(json.load(output_file), sort_keys=True)
+        with open("unitTest/eperformanceBenchmark.json", 'r') as expected_file:
+            expected_data = json.dumps(json.load(expected_file), sort_keys=True)
+        self.assertEqual(expected_data, output_data)
+      
+    def test_FrameworkParser(self):
+        parser = FrameworkParser("unitTest/framework.log","unitTest/oframework.json","unitTest/oframework.csv")
+        parser.toJsonData()
+        parser.toJsonfile()
+        output_data = json.dumps(parser.toJsonData(), sort_keys=True)
+        with open("unitTest/oframework.json", 'r') as output_file:
+            output_data = json.dumps(json.load(output_file), sort_keys=True)        
+        with open("unitTest/eframework.json", 'r') as expected_file:
+            expected_data = json.dumps(json.load(expected_file), sort_keys=True)
+        self.assertEqual(expected_data, output_data)
+
+    def test_MigrationEngineParser(self):
+        parser = MigrationEngineParser("unitTest/migrationEngine.log","unitTest/omigrationEngine.json","unitTest/omigrationEngine.csv")
+        parser.toJsonData()
+        parser.toJsonfile()
+        output_data = json.dumps(parser.toJsonData(), sort_keys=True)
+        with open("unitTest/omigrationEngine.json", 'r') as output_file:
+            output_data = json.dumps(json.load(output_file), sort_keys=True)
+        with open("unitTest/emigrationEngine.json", 'r') as expected_file:
+            expected_data = json.dumps(json.load(expected_file), sort_keys=True)
+        self.assertEqual(expected_data, output_data)
+
+if __name__ == '__main__':
+    unittest.main()
