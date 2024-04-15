77dd941addb28f7ba9bb64d8076a23ecc3c7fee5
Fares <hamoudaferes@gmail.com>
2024-04-15 15:24:15 +0000

    change logs parser

---
+++ deployment/reporter/logsParser/db2migrationEngineParser.py
@@ -0,0 +1,76 @@
+from myParser import Parser
+import re
+import json
+import csv
+import traceback
+import configparser
+from pymongo import MongoClient
+import pandas as pd
+class Db2MigrationEngineParser(Parser):
+    def __init__(self,log_file_path,json_file_path,csv_file_name):
+
+        super().__init__(log_file_path,json_file_path,csv_file_name)
+        config = configparser.ConfigParser()
+        config.comment_prefixes = (';',)  # Set the prefix character for comments
+        config.read('config.ini')
+
+        self.collection_name = config.get('mongo', 'migrationEnginelogsCollection_name')
+        # Define a regular expression pattern to extract the key and key-value pairs
+        self.log_pattern = r'Key=(.*?), Value=(.*?)$'
+        self.header = ['Experiment Number','compress','maxStreams','sourceDatabasetoTargetDatabase','tables','Experiment startTime','logTime','logType','logLocation','logDescription']
+        
+    def toJsonData(self):
+               # Compile the regular expression
+        self.data = []
+        
+        extracted_dict = {}
+        log_regex = re.compile(self.log_pattern)
+        try:
+            # Open the log file for reading
+            with open(self.log_file_path, "r") as log_file:
+                # Read and process each line in the log file
+                for line in log_file:
+                    # Find the key and key-value pairs in the log line
+                    match = log_regex.search(line)
+
+                    if match:
+                        key = match.group(1).split('-')  # Extract the key
+                        value =  match.group(2)
+                        value_pairs = match.group(2).split(' | ')  # Split key-value pairs into a list
+
+                        # Create a dictionary to store the extracted key-value pairs
+                        extracted_dict = {}
+                        for i in range(0,6):
+                            extracted_dict[self.header[i]] = key[i]
+                        if (len(value_pairs)>1):
+                            for i in range(6,len(self.header)):
+                                extracted_dict[self.header[i]] = value_pairs[i - 6].strip()
+                        else : 
+                            extracted_dict['description'] = value
+
+                    self.data.append(extracted_dict)
+            
+            return extracted_dict
+        except FileNotFoundError:
+            print(f"File '{self.log_file_path}' not found.")
+        except IOError as e:
+            print(f"Error reading the file: {e}") 
+
+    def toCSVfile(self):
+        try:
+            with open(self.csv_file_path, 'w', encoding='UTF8') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(self.header)
+                for row in self.data:
+                    values = list(row.values())
+
+                    if (len(values)<len(self.header)):
+                        commas = ([''] * (len(self.header) - len(values))) 
+                        values = values[:-1] + commas  + [values[-1]]
+                        
+                                         
+
+                    writer.writerow(values)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            traceback.print_exc()     
--- deployment/reporter/logsParser/main.py
+++ deployment/reporter/logsParser/main.py
@@ -2,19 +2,43 @@ from frameworkParser import FrameworkParser
 from performanceBenchmarkParser import PerformanceBenchmarkParser
 from db2PerformanceBenchmarkParser import Db2PerformanceBenchmarkParser
 from migrationEngineParser import MigrationEngineParser
+from db2migrationEngineParser import Db2MigrationEngineParser
+import configparser
 import os
 # Get the directory of the currently running script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Change the working directory to the script's directory
 os.chdir(script_dir)
-parsers = []
-parsers.append(Db2PerformanceBenchmarkParser("../kafkacluster/performanceBenchmark.log","performanceBenchmark.json","performanceBenchmark.csv"))
-parsers.append(FrameworkParser("../kafkacluster/framework.log","framework.json","framework.csv"))
-parsers.append(MigrationEngineParser("../kafkacluster/migrationEngine.log","migrationEngine.json","migrationEngine.csv"))
+config = configparser.ConfigParser()
+config.comment_prefixes = (';',)  # Set the prefix character for comments
+config.read('config.ini')
+migrationEngineName = config.get('migrationEngine', 'name')
 
+
+parsers = []
+parsers.append(FrameworkParser("../kafkacluster/framework.log","framework.json","framework.csv"))
+
+# Define the path of the folder containing data files 
+
+if migrationEngineName.lower() == "db2":
+    print("We'll use DB2 logs Parser as specified in config.ini ! ")
+    parsers.append(Db2PerformanceBenchmarkParser("../kafkacluster/performanceBenchmark.log","performanceBenchmark.json","performanceBenchmark.csv"))
+    parsers.append(Db2MigrationEngineParser("../kafkacluster/migrationEngine.log","migrationEngine.json","migrationEngine.csv"))
+elif migrationEngineName.lower() =="default":
+    print("We'll use Default Engine logs Parser as specified in config.ini ! ")
+    parsers.append(PerformanceBenchmarkParser("../kafkacluster/performanceBenchmark.log","performanceBenchmark.json","performanceBenchmark.csv"))
+    parsers.append(MigrationEngineParser("../kafkacluster/migrationEngine.log","migrationEngine.json","migrationEngine.csv"))
+elif migrationEngineName.lower() == "fivetran":
+    print("We'll use Fivetran logs Parser as specified in config.ini ! ")
+    parsers.append(PerformanceBenchmarkParser("../kafkacluster/performanceBenchmark.log","performanceBenchmark.json","performanceBenchmark.csv"))
+    parsers.append(MigrationEngineParser("../kafkacluster/migrationEngine.log","migrationEngine.json","migrationEngine.csv"))
+
+print("start Parsing ! ")
 for parser in parsers:
     parser.toJsonData()
     parser.toJsonfile()
     parser.toCSVfile()
     parser.SavetoDB()
+
+print("Parsing done !")
